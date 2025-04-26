64e0ca1678ec384d752f51c51ed790389ce4288b
kwamentw <antwi1496@hotmail.com>
2025-04-26 18:32:59 -0400

    AutoSplit setup

---
+++ src/AutoSplit.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+//import Token
+
+contract AutoSplit {
+    //TokenA
+    //TokenB
+
+    //deposit
+    //rebalance
+    //swap
+    //withdraw
+
+}
---
+++ test/SplitTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {AutoSplit} from "../src/AutoSplit.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract SplitTest is Test{
+    
+}
